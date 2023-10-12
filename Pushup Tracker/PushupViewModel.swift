463cddb012e32d9049484700e00a41d5e10d4278
ylehilds <lehi_alcantara@byu.edu>
2023-10-12 09:01:42 -0600

    modified dateKey method

--- Pushup Tracker/PushupViewModel.swift
+++ Pushup Tracker/PushupViewModel.swift
@@ -17,6 +17,7 @@ import Foundation
         static let maxDailyPushupsForKyle = 50
         static let maxDateValueForKyle =  11 * maxDaysPerMonth + 10
         static let minDateValueForKyle =  8 * maxDaysPerMonth + 3
+        static let secondsPerDay:TimeInterval =  60 * 60 * 24
     }
     
     // MARK: - Properties
@@ -26,9 +27,47 @@ import Foundation
     // MARK: - Model access
     
     var pushupsThatCount: Int {
+        var counts = [String: (count: Int, excess: Int, date: Date)]()
+        
         pushupData.workouts
             .filter { dateCountsForKyle($0.date) }
-            .reduce(0) { $0 + min($1.count,Constants.maxDailyPushupsForKyle) }
+            .forEach { workoutReport in
+                let key = dateKey(for: workoutReport.date)
+                
+                counts[key] = (max(workoutReport.count, Constants.maxDailyPushupsForKyle),
+                               max(workoutReport.count - Constants.maxDailyPushupsForKyle, 0),
+                               workoutReport.date)
+            }
+        counts.keys.sorted().forEach{ key in
+            guard let value = counts[key] else {
+                return
+            }
+            
+            let excess = value.excess
+            let date = value.date
+            
+            if excess > 0 {
+                let subsequentDay = date.addingTimeInterval(Constants.secondsPerDay)
+                let previousDay = date.addingTimeInterval(-Constants.secondsPerDay)
+                let subsequentKey = dateKey(for: subsequentDay)
+                let previousKey = dateKey(for: previousDay)
+                
+                if dateCountsForKyle(subsequentDay) && counts[subsequentKey] == nil {
+                    counts[subsequentKey] = (min(excess, Constants.maxDailyPushupsForKyle), 0, subsequentDay)
+                } else if dateCountsForKyle(previousDay) && counts[previousKey] == nil {
+                    counts[previousKey] = (min(excess, Constants.maxDailyPushupsForKyle), 0, previousDay)
+                } else if let subsequentValue = counts[subsequentKey],
+                          dateCountsForKyle(subsequentDay),
+                          subsequentValue.count < Constants.maxDailyPushupsForKyle {
+                    counts[subsequentKey] = (min(subsequentValue.count + excess, Constants.maxDailyPushupsForKyle), 0, subsequentDay)
+                } else if let previousValue = counts[previousKey],
+                          dateCountsForKyle(previousDay),
+                          previousValue.count < Constants.maxDailyPushupsForKyle {
+                    counts[previousKey] = (min(previousValue.count + excess, Constants.maxDailyPushupsForKyle), 0, previousDay)
+                }
+            }
+        }
+        return counts.reduce(0) { $0 + min($1.value.count, Constants.maxDailyPushupsForKyle)}
     }
     
     var totalCount: Int {
@@ -75,5 +114,13 @@ import Foundation
         return dateValue >= Constants.minDateValueForKyle
             && dateValue <= Constants.maxDateValueForKyle
             && year == currentYear
+    }
+    
+    func dateKey(for date: Date) -> String {
+        let components = Calendar.current.dateComponents([.month, .day], from: date)
+        guard let month = components.month, let day = components.day else {
+            return ""
+        }
+        return "\(month)-\(day)"
     }
 }
